54263adbcdc535a4f24e60748664062252692f72
Maxime Mouchet <max@maxmouchet.com>
2022-03-09 14:18:26 +0100

    scripts: ignore time differences

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-fast-mda-traceroute --format=scamper-json $* | jq > fast_mda_traceroute.jsonl
-eval "$(fast-mda-traceroute --print-command=scamper $*)" | jq > scamper.jsonl
+fast-mda-traceroute --format=scamper-json $* | jq | egrep -v "sec|time" > fast_mda_traceroute.jsonl
+eval "$(fast-mda-traceroute --print-command=scamper $*)" | jq | egrep -v "sec|time" > scamper.jsonl
 
 diff --expand-tabs --minimal --side-by-side scamper.jsonl fast_mda_traceroute.jsonl > diff
