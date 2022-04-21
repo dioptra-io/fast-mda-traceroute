848ae3bdfa74a3880e9d0a0823948165e4fb5a40
Maxime Mouchet <max@maxmouchet.com>
2022-04-21 14:34:47 +0200

    tests: test --print-command and --version

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,9 +5,23 @@ from fast_mda_traceroute.cli import app
 
 
 @pytest.mark.parametrize("format", ["table", "traceroute", "scamper-json"])
-def test_cli_text(format):
+def test_cli_basic(format):
     runner = CliRunner()
     result = runner.invoke(
         app, [f"--format={format}", "--log-level=debug", "example.org"]
     )
     assert result.exit_code == 0
+
+
+@pytest.mark.parametrize("command", ["paris-traceroute", "scamper"])
+def test_cli_command(command):
+    runner = CliRunner()
+    result = runner.invoke(app, [f"--print-command={command}", "example.org"])
+    assert result.exit_code == 0
+
+
+def test_cli_version():
+    runner = CliRunner()
+    result = runner.invoke(app, ["--version"])
+    assert result.exit_code == 0
+    assert result.stdout.startswith("fast-mda-traceroute")
