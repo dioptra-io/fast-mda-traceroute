c6b5ad6f4b681d4ebcc0f031310984936af7dcf2
Maxime Mouchet <max@maxmouchet.com>
2022-04-21 16:41:22 +0200

    formats: fix traceroute format

--- fast_mda_traceroute/formats/__init__.py
+++ fast_mda_traceroute/formats/__init__.py
@@ -1,4 +1,5 @@
 from fast_mda_traceroute.formats.scamper import format_scamper_json
 from fast_mda_traceroute.formats.table import format_table
+from fast_mda_traceroute.formats.traceroute import format_traceroute
 
-__all__ = ("format_scamper_json", "format_table")
+__all__ = ("format_scamper_json", "format_table", "format_traceroute")
--- fast_mda_traceroute/formats/traceroute.py
+++ fast_mda_traceroute/formats/traceroute.py
@@ -15,7 +15,7 @@ def format_traceroute(replies: List[Reply]) -> str:
     if destination_replies:
         destination_ttl = min(x.probe_ttl for x in destination_replies)
     nodes_by_ttl = map_reduce(
-        replies, lambda x: x.probe_ttl, lambda x: x.probe_ttl, set
+        replies, lambda x: x.probe_ttl, lambda x: x.reply_src_addr, set
     )
     output = ""
     for ttl in range(min(nodes_by_ttl), min(max(nodes_by_ttl), destination_ttl) + 1):
